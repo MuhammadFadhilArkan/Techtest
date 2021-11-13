e666df14686f33650c57b63b62c75c3e4baa4788
MuhammadFadhilArkan <68679339+MuhammadFadhilArkan@users.noreply.github.com>
2021-11-13 11:07:31 +0700

    Update Q3_application.py

--- Q3/Transformers/Q3_application.py
+++ Q3/Transformers/Q3_application.py
@@ -5,7 +5,7 @@ class pipeLine:
     def __init__(self):
 
         print("Preparing the model...")
-        self.model_name = "fadhilarkan/distilbert-base-uncased-finetuned-cola-3"
+        self.model_name = "fadhilarkan/distilbert-base-uncased-finetuned-cola-4"
         self.model = pipeline('text-classification', model=self.model_name, tokenizer=self.model_name)
         self.label_dict = {"LABEL_0":"AIMX", "LABEL_1":"OWNX", "LABEL_2":"CONT", "LABEL_3":"BASE", "LABEL_4":"MISC"}
 
